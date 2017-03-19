afdf8731149e28b55a8bd1d375e02f8180f87b18
Yari Yakup <yariyakup@gmail.com>
2017-03-19 00:22:45 -0400

    update controller

--- src/main/java/com/uyghurbiz/controller/TweetServiceController.java
+++ src/main/java/com/uyghurbiz/controller/TweetServiceController.java
@@ -13,6 +13,9 @@ import twitter4j.TwitterException;
 import twitter4j.User;
 import twitter4j.api.UsersResources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by yyakup on 3/15/17.
  */
@@ -23,8 +26,12 @@ public class TweetServiceController {
 
     @RequestMapping("/")
     @ResponseBody
-    public String home() {
-        return "Hello World!";
+    public List<String> home() {
+        List<String> listOfService = new ArrayList<String>();
+        listOfService.add("/user");
+        listOfService.add("/trends");
+        listOfService.add("/trend");
+        return listOfService;
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
