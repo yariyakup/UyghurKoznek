46fa72ba058331598a41f378bd243e8d3034cf58
yyakup <yyakup@paypal.com>
2017-03-26 12:19:06 -0400

    updated desc

--- src/main/java/com/uyghurbiz/controller/HomeController.java
+++ src/main/java/com/uyghurbiz/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.uyghurbiz.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by yyakup on 3/26/17.
  */
 @RestController
+@Api(hidden = true)
 public class HomeController {
     @RequestMapping("/")
     public String home() {
